919e8d5201d766a266b953659216869da8baf7c4
Christian <c.herrera14@ufromail.cl>
2023-03-20 21:35:33 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 }
 
 module.exports = {
-  eslint: false,
   env: {
     TOKEN: process.env.REACT_APP_TOKEN,
     METAR_TOKEN: process.env.REACT_APP_METAR
